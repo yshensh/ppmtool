dce42cad31af26ffad81d36b8eccf5f0f63c69e6
sheny <yi.shen@blackrock.com>
2020-02-25 18:00:15 -0500

    wire up controller and service

---
+++ src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package com.example.ppmtool.services;
+
+import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdate(Project project) {
+
+        // TODO: other logics
+
+        return projectRepository.save(project);
+    }
+
+}
