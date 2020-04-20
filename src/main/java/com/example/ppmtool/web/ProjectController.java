20079d7991fd09ff2f54515120913e3333ba750f
Yi Shen <yshensh@gmail.com>
2020-04-19 22:55:11 -0400

    update readme

--- src/main/java/com/example/ppmtool/web/ProjectController.java
+++ src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
