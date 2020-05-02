9228008b41b0a4702c2076068bc3b4b652250210
Yi Shen <yshensh@gmail.com>
2020-05-02 17:14:57 -0400

    wire up backlog and project relationship

---
+++ src/main/java/com/example/ppmtool/domain/Backlog.java
@@ -0,0 +1,58 @@
+package com.example.ppmtool.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Backlog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence = 0;
+    private String projectIdentifier;
+
+    // one-to-one with project
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="project_id", nullable = false)
+    @JsonIgnore
+    private Project project;
+
+    // one-to-many project tasks
+
+    public Backlog() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSequence() {
+        return PTSequence;
+    }
+
+    public void setPTSequence(Integer PTSequence) {
+        this.PTSequence = PTSequence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
--- src/main/java/com/example/ppmtool/services/ProjectService.java
+++ src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.example.ppmtool.services;
 
+import com.example.ppmtool.domain.Backlog;
 import com.example.ppmtool.domain.Project;
 import com.example.ppmtool.exceptions.ProjectIdException;
+import com.example.ppmtool.repositories.BacklogRepository;
 import com.example.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,29 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdate(Project project) {
 
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            // create backlog only when a new project is created
+            if(project.getId()==null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId() != null) {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+
+            }
+
             return projectRepository.save(project);
+
         } catch (Exception e) {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
