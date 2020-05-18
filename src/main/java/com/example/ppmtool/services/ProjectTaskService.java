fb11620c97954e0db1bced1675d185b6212a4032
Yi Shen <yshensh@gmail.com>
2020-05-17 21:13:21 -0400

    find ProjectTask by projectSequence

--- src/main/java/com/example/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/example/ppmtool/services/ProjectTaskService.java
@@ -60,7 +60,7 @@ public class ProjectTaskService {
         Project project = projectRepository.findByProjectIdentifier(id.toUpperCase());
 
         if (project==null) {
-            throw new ProjectNotFoundException("Project with id '" + id + "' does not exit");
+            throw new ProjectNotFoundException("Project with ID '" + id + "' does not exit");
         }
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
@@ -69,8 +69,24 @@ public class ProjectTaskService {
     public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 
         // make sure we are searching on the right backlog
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if(backlog==null) {
+            throw new ProjectNotFoundException("Project with ID '" + backlog_id + "' does not exit");
+        }
 
-        return projectTaskRepository.findByProjectSequence(pt_id);
+        // make sure that our task exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        if(projectTask==null) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+        }
 
+        // make sure that the backlog/project id in the path corresponds to the right project
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: '" + backlog_id);
+        }
+
+        return projectTask;
     }
+
+
 }
