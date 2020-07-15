59ca688dc4aa55867e84c9974e97875bbe6f746e
Yi Shen <yshensh@gmail.com>
2020-07-15 16:01:33 -0400

    user registration and generate JWT token

--- src/main/java/com/example/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/example/ppmtool/services/ProjectTaskService.java
@@ -11,8 +11,6 @@ import com.example.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 
 @Service
 public class ProjectTaskService {
