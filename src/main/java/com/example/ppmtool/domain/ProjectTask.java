05742f70b97a5c1c1ec89df233c8bf564ced68c2
Yi Shen <yshensh@gmail.com>
2020-07-20 15:10:19 -0400

    minor bug fix on dueDate format

--- src/main/java/com/example/ppmtool/domain/ProjectTask.java
+++ src/main/java/com/example/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,6 @@
 package com.example.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
@@ -19,6 +20,7 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private String status;
     private Integer priority;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date dueDate;
 
     // many-to-one with Backlog
